0a1f05f578685bbdabb2a5f1025caf731609d0ed
Daniel Uličný <dankoulicny11@gmail.com>
2026-05-31 09:27:56 +0200

    Closures

--- script.js
+++ script.js
@@ -1,9 +1,30 @@
-// CALLBACK
-function printGreeting(firstName, lastName, callback) {
-  let fullName = firstName + " " + lastName;
-  console.log(callback(fullName));
+function outer(a) {
+  // This function returns another function
+  function inner(b) {
+    console.log(a); // From outer scope (outer function)
+    console.log(b); // From current scope (inner function)
+  }
+
+  return inner; // Return the inner function
 }
-function getGreeting(name) {
-  return "Hello " + name;
+
+// Create a new function by calling outer
+const newFunc = outer(1);
+
+// Call the returned function
+newFunc(2);
+// Output:
+// 1
+// 2
+
+// INTERVIEW QUESTION
+function outer(x) {
+  function inner(y) {
+    console.log(x + y);
+  }
+  return inner;
 }
-printGreeting("Daniel", "Ulicny", getGreeting);
+
+const addFive = outer(5);
+addFive(3); // What does this print?
+// 8
